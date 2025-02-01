2bf612b45f66450c96cb96b47e710dbe73eee2a3
Scott Westover <scottwestover2006@gmail.com>
2025-02-01 14:24:12 -0500

    added initial project base
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Level from "./scenes/Level.js";
+import Preload from "./scenes/Preload.js";
+
+window.addEventListener('load', function () {
+
+	var game = new Phaser.Game({
+		width: 256,
+		height: 224,
+		type: Phaser.AUTO,
+        backgroundColor: "#242424",
+		scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH
+		}
+	});
+
+	game.scene.add("Level", Level);
+	game.scene.add("Preload", Preload, true);
+});
---
+++ src/scenes/Preload.js
@@ -0,0 +1,53 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Preload extends Phaser.Scene {
+
+	constructor() {
+		super("Preload");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorPreload() {
+
+		this.load.pack("asset-pack", "assets/asset-pack.json");
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		this.events.emit("scene-awake");
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here
+
+	preload() {
+
+		this.editorCreate();
+
+		this.editorPreload();
+
+	}
+
+	create() {
+
+		this.scene.start("Level");
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
